c66800d11a5b5d8326c6559a92d2cd7a8116563c
vaniafed <ztankist@gmail.com>
2023-10-10 15:14:00 +0300

    tmp

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("tmp/db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
