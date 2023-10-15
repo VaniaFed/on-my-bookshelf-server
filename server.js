bb9bc5e195369a635388722783b7bd8d51524949
vaniafed <ztankist@gmail.com>
2023-10-15 15:41:01 +0300

    ssss

--- server.js
+++ server.js
@@ -12,17 +12,9 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-server.use(
-  jsonServer.rewriter({
-    "/*": "/$1",
-    "/books/:resource/:id/show": "/:resource/:id",
-  })
-);
-
 server.use(router);
 
-server.listen(3001, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
